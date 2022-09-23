ae0df06993ffab1443a49f571baf68d6866b6052
Isa Sierra <siemeris@gmail.com>
2022-09-23 19:17:12 +0200

    cart

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { AiOutlineShopping } from 'react-icons/ai'
 
+import { Cart } from "./";
+import { useStateContext } from '../context/StateContext';
+
 
 const Navbar = () => {
+    const { showCart, setShowCart, totalQuantities } = useStateContext();
   return (
     <>
 
@@ -58,8 +62,9 @@ const Navbar = () => {
 						</ul>
                         <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
         <AiOutlineShopping />
-        <span className="cart-item-qty">1</span>
+        <span className="cart-item-qty">{totalQuantities}</span>
       </button>
+      { showCart && <Cart />}
 
 					</div>
 
