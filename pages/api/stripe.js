a0b46e91aedd59baec793b94bc35acb812d42748
siemeris <siemeris@gmail.com>
2022-09-24 08:18:20 +0000

    funcionando OK con los pagos

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
         ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
-          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/m789dqq2/production/').replace('-webp', '.webp');
+          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/m789dqq2/production/').replace('-jpg', '.jpg');
 
           return {
             price_data: { 
