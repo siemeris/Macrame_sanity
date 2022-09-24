c784b4ec2d9018a3974312b75490298dbe149c3c
siemeris <siemeris@gmail.com>
2022-09-24 19:13:16 +0000

    shipping address

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
         mode: 'payment',
         payment_method_types: ['card'],
         shipping_address_collection:{
-          'allowed_countries': ['US', 'ES'],
+          'allowed_countries': ['ES'],
         },
         billing_address_collection: 'auto',
         shipping_options: [
